39584ac3a4735e725a615fd5a0483374d2f8fae1
Jose Manuel <josediazrdt@gmail.com>
2023-11-03 16:40:47 +0000

    cosas php

--- src/models/Car.php
+++ src/models/Car.php
@@ -1,11 +1,14 @@
 <?php
-
+namespace Jose\Cars\Models;
 class Car {
+  private static $cars = [];
+  
   public $id;
   public $make;
   public $model;
   public $year;
   public $color;
+  
 
   public function __construct($id, $make, $model, $year, $color)
   {
@@ -14,6 +17,40 @@ class Car {
     $this->model = $model;
     $this->year = $year;
     $this->color = $color;
+
+    
   }
 
+  public static function getAll() { 
+  $cars = [];
+    $json = file_get_contents('../data/cars.json');
+    $carsJSON = json_decode($json);
+    foreach ($carsJSON as $carJSON) {
+      $cars[] = new Car($carJSON->id,$carJSON->make,$carJSON->model,$carJSON->year,$carJSON->color);
+    }
+    return $cars;
+  }
+
+  public static function find($id){
+    foreach(self::getAll() as $car){
+      if ($car->id == $id) return $car;
+    }
+    return null;
+  }
+
+  public static function delete($id){
+    echo "Borrando el coche con id=$id desde Car";
+    $cars = [];
+    foreach (self::getAll() as $car){
+      if($car -> id != $id ){
+        $cars[] = $car;
+      }
+    }
+    // Convert JSON data from an array to a string
+    $jsonString = json_encode($cars, JSON_PRETTY_PRINT);
+    // Write in the file
+    $file = fopen('../data/cars.json', 'w');
+    fwrite($file, $jsonString);
+    fclose($file);
+  }
 }
